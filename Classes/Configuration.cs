7e04bc57a8fa6c7f67a1a6b735d7173ccb3ee9a8
Christopher Boisvert <administrateur@greenwoodmultimedia.com>
2021-09-12 00:14:31 -0400

    Classe GestionAes, raccourcis commandes, encryption date dern. connexion
    La classe permet de mieux diviser le code qui gÃ¨re l'encryption et la Ã©cryptio. Les raccourcis commandes permette d'utiliser le programme sans devoir taper autant de caractÃre. Afin de garantir que la date de derniere connexion ne soit pas corrompu, celle-ci sera a l'avenir encryptÃ.

---
+++ Classes/Configuration.cs
@@ -0,0 +1,43 @@
+﻿namespace gw_pass
+{
+    /// <summary>
+    /// Représente la configuration de gw_pass.
+    /// </summary>
+    class Configuration
+    {
+        /// <summary>
+        /// Version du programme.
+        /// </summary>
+        public string version { get; set; }
+
+        /// <summary>
+        /// Représente le courriel de l'utilisateur.
+        /// </summary>
+        public string courriel { get; set; }
+
+        /// <summary>
+        /// Représente le mot de passe de l'utilisateur.
+        /// </summary>
+        public string mot_de_passe { get; set; }
+
+        /// <summary>
+        /// Sel ajouté à l'encryption.
+        /// </summary>
+        public byte[] sel { get; set; }
+
+        /// <summary>
+        /// Indique la dernière fois que la clé de décryption a bel et bien été entré.
+        /// </summary>
+        public string date_initialisation { get; set; }
+
+        /// <summary>
+        /// Indique la dernière fois que la clé de décryption a bel et bien été entré.
+        /// </summary>
+        public string derniere_date_acces { get; set; }
+
+        /// <summary>
+        /// Liste des services enregistrés.
+        /// </summary>
+        public string liste_service { get; set; }
+    }
+}
---
+++ Classes/GestionAes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gw_pass.Classes
+{
+    class GestionAes
+    {
+        private SecureString cle;
+        private byte[] sel;
+        private SecureString secureString;
+
+        public GestionAes(SecureString cle, byte[] sel)
+        {
+            this.cle = cle;
+            this.sel = sel;
+        }
+
+        public GestionAes(SecureString secureString)
+        {
+            this.secureString = secureString;
+        }
+
+        /// <summary>
+        /// Encrypte des données en utilisant l'algorithme AES en mode CBC.
+        /// </summary>
+        /// <param name="donnees">Données sous format "string" à encrypter.</param>
+        /// <returns>Retourne les données sous forme encrypté et en format base 64.</returns>
+        public string encrypter(string donnees)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(donnees);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(sha_256(cle), sel, 1000, HashAlgorithmName.SHA256);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    donnees = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return donnees;
+        }
+
+
+        /// <summary>
+        /// Décrypte des données en utilisant l'algorithme AES en mode CBC.
+        /// </summary>
+        /// <param name="donnees">Données sous format "string" à décrypter.</param>
+        /// <returns>Retourne les données décryptées et en format Unicode.</returns>
+        public string decrypter(string donnees)
+        {
+            byte[] cipherBytes = Convert.FromBase64String(donnees);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(sha_256(cle), sel, 1000, HashAlgorithmName.SHA256);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    donnees = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return donnees;
+        }
+
+        /// <summary>
+        /// Hash un mot de passe en sha256.
+        /// </summary>
+        /// <param name="text">Le mot de passe à hashé sous forme de Secure String.</param>
+        /// <returns>Retourne le hash.</returns>
+        public static string sha_256(SecureString mot_de_passe)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(new NetworkCredential(string.Empty, mot_de_passe).Password);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
